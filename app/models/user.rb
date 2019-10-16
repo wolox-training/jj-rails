ef2e2a1114c856ca7c3f46a7b8b1261f1914ac4e
erosennin93 <julian.salas92@gmail.com>
2019-10-16 16:34:52 -0300

    one line validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, :last_name, :presence => true
 end
