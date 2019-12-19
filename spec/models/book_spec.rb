6d61a7c1a1fc377d02a6bb723c6b72b2ca833c9e
erosennin93 <julian.salas92@gmail.com>
2019-12-19 12:24:31 -0300

    change the let for subject

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Book do
-  let(:book) { build(:book) }
+  subject(:book) { build(:book) }
 
   it { is_expected.to be_valid }
   it { is_expected.to validate_presence_of(:gender) }
