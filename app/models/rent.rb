24bc43be68c304fec57fb9a1aaba23cfa0492e5e
erosennin93 <julian.salas92@gmail.com>
2019-12-27 13:03:36 -0300

    fix offenses

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -5,7 +5,6 @@ class Rent < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-
   def begin_date_greater_than_end_date
     errors.add(:begin_date, 'begin_date cannot be greater than end_date') if begin_date > end_date
   end
