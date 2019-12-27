d956b845319a469a5b1869eca247658a5aae87f6
erosennin93 <julian.salas92@gmail.com>
2019-12-27 09:36:19 -0300

    add custom validations

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,4 +1,18 @@
 class Rent < ApplicationRecord
   validates :user, :book, :begin, :end, presence: true
+  validate :begin_greater_than_end, :end_less_than_begin
+  
   belongs_to :user, :book
+  
+  def begin_greater_than_end
+    if begin > end
+      errors.add(:begin, "begin date cannot be greater than end date")
+    end
+  end
+
+  def end_less_than_begin
+    if end < begin
+      errors.add(:end, "end date cannot be less than begin date")
+    end
+  end
 end
