4e6eeddb60bd4c5e4e828ce1f315d9146998b789
erosennin93 <julian.salas92@gmail.com>
2019-12-27 09:39:25 -0300

    fix offenses

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,18 +1,14 @@
 class Rent < ApplicationRecord
   validates :user, :book, :begin, :end, presence: true
   validate :begin_greater_than_end, :end_less_than_begin
-  
+
   belongs_to :user, :book
-  
+
   def begin_greater_than_end
-    if begin > end
-      errors.add(:begin, "begin date cannot be greater than end date")
-    end
+    errors.add(:begin, 'begin date cannot be greater than end date') if :begin > :end
   end
 
   def end_less_than_begin
-    if end < begin
-      errors.add(:end, "end date cannot be less than begin date")
-    end
+    errors.add(:end, 'end date cannot be less than begin date') if :end < :begin
   end
 end
