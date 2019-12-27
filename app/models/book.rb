fc6dfc491534158796305c633067436f56355d60
Julian Salas Perilli <juliansalasperilli@gmail.com>
2019-12-27 14:20:01 -0300

    Rent model (#9)
    Add Rent model and its migrations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   validates :gender, :author, :image, :title, :editor, :year, presence: true
+
+  has_many :rents
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+    user = User.create(email: 'julian.salas@wolox.com.ar', password: '12345678', first_name: 'Julian', last_name: 'Salas Perilli')
+    
+    book = Book.create(gender: 'Detective fiction', author: 'Sir Arthur Conan Doyle', image: 'https://google.com.ar', title: 'A Study in Scarlet', editor: 'Ward Lock & Co', year: '1887')
+
+    Rent.create(user: user, book: book, begin_date: '2019-12-19', end_date: '2019-12-21')
+end
