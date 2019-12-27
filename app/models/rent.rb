207061cef606385d8bd0cb14c12017193073e79d
erosennin93 <julian.salas92@gmail.com>
2019-12-27 13:58:43 -0300

    remove unnecesary validation

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,6 +1,6 @@
 class Rent < ApplicationRecord
   validates :user, :book, :begin_date, :end_date, presence: true
-  validate :begin_date_greater_than_end_date, :end_date_less_than_begin_date
+  validate :begin_date_greater_than_end_date
 
   belongs_to :user
   belongs_to :book
@@ -9,9 +9,5 @@ class Rent < ApplicationRecord
 
   def begin_date_greater_than_end_date
     errors.add(:begin_date, 'begin_date cannot be greater than end_date') if begin_date > end_date
-  end
-
-  def end_date_less_than_begin_date
-    errors.add(:end_date, 'end_date cannot be less than begin_date') if end_date < begin_date
   end
 end
