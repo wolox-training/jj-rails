4520c0c46dcb1fb6d586585fe000a35d3eb790c8
erosennin93 <julian.salas92@gmail.com>
2020-01-03 14:00:34 -0300

    add eager_loading

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -2,18 +2,20 @@ class RentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render_paginated current_user.rents
+    render_paginated current_user.rents.includes(:book).all
   end
 
   def create
-    @rent = Rent.new(permitted_params)
-    if @rent.save
-      render json: @rent, status: :created
+    rent = Rent.new(permitted_params)
+    if rent.save
+      render json: rent, status: :created
     else
-      render json: { errors: @rent.errors.messages }, status: :unprocessable_entity
+      render json: { errors: rent.errors.messages }, status: :unprocessable_entity
     end
   end
 
+  private
+  
   def permitted_params
     params
       .require(:rent)
--- app/serializers/rent_serializer.rb
+++ app/serializers/rent_serializer.rb
@@ -1,6 +1,5 @@
 class RentSerializer < ActiveModel::Serializer
   attributes :id, :begin_date, :end_date
 
-  belongs_to :user
   belongs_to :book
 end
