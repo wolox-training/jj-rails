a754b2b91d4ca76fe76850c46b93ef8f510e8c91
erosennin93 <julian.salas92@gmail.com>
2020-01-09 18:06:48 -0300

    move :set_locale to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Wor::Paginate
 
+  before_action :set_locale
+
   rescue_from ActiveRecord::RecordNotFound do |_exception|
     render json: { "message": 'The requested resource was not found' }, status: :not_found
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = current_user&.locale || I18n.default_locale
   end
 end
