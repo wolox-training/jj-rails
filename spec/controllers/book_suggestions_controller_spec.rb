37601ee6b3e01b9d93b0c9fe67526862574698b6
erosennin93 <julian.salas92@gmail.com>
2020-02-28 12:38:20 -0300

    add tests & remove others

--- spec/controllers/book_suggestions_controller_spec.rb
+++ spec/controllers/book_suggestions_controller_spec.rb
@@ -5,7 +5,7 @@ describe BookSuggestionsController do
 
   describe 'POST #create' do
     let(:book_suggestion) { build(:book_suggestion) }
-    let!(:http_request) { post :create, params: params }
+    let(:http_request) { post :create, params: params }
 
     context 'with an authenticated user' do
       include_context 'with an authenticated user'
@@ -14,75 +14,55 @@ describe BookSuggestionsController do
         let(:params) { {} }
 
         it 'responds with 422 status' do
-          expect(http_request).to have_http_status(:unprocessable_entity)
+          http_request
+          expect { http_request }.to have_http_status(:unprocessable_entity)
         end
       end
 
       context 'with the required params' do
         let(:params) { { book_suggestion: attributes_for(:book_suggestion) } }
 
+        it 'creates a new book suggestion' do
+          expect { http_request }.to change { BookSuggestion.count }.by(1)
+        end
+
         it 'responds with 201 status' do
-          expect(response).to have_http_status(:created)
-        end
-      end
-
-      context 'with an invalid author' do
-        context 'with author missing param' do
-          let(:params) { attributes_for(:book_suggestion).except(:author) }
-
-          it 'responds with 400 status' do
-            expect(response).to have_http_status(:unprocessable_entity)
-          end
-        end
-
-        context 'with author nil value' do
-          let(:params) { attributes_for(:book_suggestion, author: nil) }
-
-          it 'responds with 400 status' do
-            expect(response).to have_http_status(:unprocessable_entity)
-          end
-        end
-      end
-
-      context 'with an invalid title' do
-        let(:params) { attributes_for(:book_suggestion, title: nil) }
-
-        it 'responds with 400 status' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
-      end
-
-      context 'with an invalid url' do
-        let(:params) { attributes_for(:book_suggestion, url: nil) }
-
-        it 'responds with 400 status' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
-      end
-
-      context 'with an invalid editor' do
-        let(:params) { attributes_for(:book_suggestion, editor: nil) }
-
-        it 'responds with 400 status' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
-      end
-
-      context 'with an invalid year' do
-        let(:params) { attributes_for(:book_suggestion, year: nil) }
-
-        it 'responds with 400 status' do
-          expect(response).to have_http_status(:unprocessable_entity)
+          http_request
+          expect { response }.to have_http_status(:created)
         end
       end
     end
 
     context 'with an invited user' do
       let(:params) { { book_suggestion: attributes_for(:book_suggestion) } }
-
+      
       it 'responds with 201 status' do
-        expect(response).to have_http_status(:created)
+        http_request
+        expect { response }.to have_http_status(:created)
       end
+
+      it 'creates a new book suggestion' do
+        expect { http_request }.to change { BookSuggestion.count }.by(1)
+      end
+
+      it 'creates without user_id' do
+        http_request
+        expect(BookSuggestion.last.user_id).to be_nil
+      end
+    end
+
+    context 'with an invalid book suggestion' do
+      let(:params) { { book_suggestion: attributes_for(:book_suggestion, title: nil) } }
+      
+      it 'responds with 422 status' do
+        http_request
+        expect { response }.to have_http_status(:unprocessable_entity)
+      end
+      
+      it 'doesn\'t create a new book suggestion' do
+        expect { http_request }.to change { BookSuggestion.count }.by(0)
+      end
+
     end
   end
 end
