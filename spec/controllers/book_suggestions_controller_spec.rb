0758cb7fd5ec9cbcbc7deba0fffb7eeb735e8ef3
erosennin93 <julian.salas92@gmail.com>
2020-02-28 12:43:44 -0300

    fix RuboCop offenses

--- spec/controllers/book_suggestions_controller_spec.rb
+++ spec/controllers/book_suggestions_controller_spec.rb
@@ -22,7 +22,7 @@ describe BookSuggestionsController do
         let(:params) { { book_suggestion: attributes_for(:book_suggestion) } }
 
         it 'creates a new book suggestion' do
-          expect { http_request }.to change { BookSuggestion.count }.by(1)
+          expect { http_request }.to change(BookSuggestion, :count).by(1)
         end
 
         it 'responds with 201 status' do
@@ -34,14 +34,14 @@ describe BookSuggestionsController do
 
     context 'with an invited user' do
       let(:params) { { book_suggestion: attributes_for(:book_suggestion) } }
-      
+
       it 'responds with 201 status' do
         http_request
         expect(response).to have_http_status(:created)
       end
 
       it 'creates a new book suggestion' do
-        expect { http_request }.to change { BookSuggestion.count }.by(1)
+        expect { http_request }.to change(BookSuggestion, :count).by(1)
       end
 
       it 'creates without user_id' do
@@ -52,16 +52,15 @@ describe BookSuggestionsController do
 
     context 'with an invalid book suggestion' do
       let(:params) { { book_suggestion: attributes_for(:book_suggestion, title: nil) } }
-      
+
       it 'responds with 422 status' do
         http_request
         expect(response).to have_http_status(:unprocessable_entity)
       end
-      
-      it 'doesn\'t create a new book suggestion' do
-        expect { http_request }.to change { BookSuggestion.count }.by(0)
-      end
 
+      it 'doesn\'t create a new book suggestion' do
+        expect { http_request }.to change(BookSuggestion, :count).by(0)
+      end
     end
   end
 end
